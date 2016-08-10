e3a5e44800d85ab84b0ad26e36f399184c732f65
mmazanec22 <melanie.mazanec@gmail.com>
2016-08-10 15:18:50 -0500

    Debug Hipstar Wars themed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,33 @@
 end
 
 #question has id, asker_id, title, content
-User.each do |user|
+User.all.each do |user|
   title = Faker::StarWars.quote
   content = Faker::Hipster.paragraph + "?"
-  user.questions << Question.new(asker_id: num, title: title, content: content)
+  user.questions << Question.new(asker_id: user.id, title: title, content: content)
 end
 
 #answer has question_id, answerer_id, content
-User.each do |user|
+User.all.each do |user|
   content = Faker::Hipster.paragraph
-  user.answers << Answer.new(question_id: [1..6].sample, answerer_id: user.id, content: content)
+  p rand(1..6)
+  user.answers << Answer.new(question_id: rand(1..6), answerer_id: user.id, content: content)
 end
 
 #comment has commentable_id, commenter_id, commentable_type, content
-User.each do |user|
+User.all.each do |user|
   3.times do
     content = Faker::StarWars.quote
-    user.comments << Comment.new(commentable_id: [1..6].sample, commenter_id: user.id, commentable_type: "question", content: content)
+    user.comments << Comment.new(commentable_id: rand(1..6), commenter_id: user.id, commentable_type: "question", content: content)
   end
   3.times do
-    Faker::StarWars.quote
-    user.comments << Comment.new(commentable_id: [1..6].sample, commenter_id: user.id, commentable_type: "answer", content: content)
+    content = Faker::StarWars.quote
+    user.comments << Comment.new(commentable_id: rand(1..6), commenter_id: user.id, commentable_type: "answer", content: content)
   end
 end
 
 #votes has votable_id, voter_id, votable_type
-User.each do |user|
-  user.votes << Vote.new([votable_id: 1..6].sample, voter_id: user.id, votable_type: "question")
-  user.votes << Vote.new([votable_id: 1..6].sample, voter_id: user.id, votable_type: "answer")
+User.all.each do |user|
+  user.votes << Vote.new(votable_id: rand(1..6), voter_id: user.id, votable_type: "question")
+  user.votes << Vote.new(votable_id: rand(1..6), voter_id: user.id, votable_type: "answer")
 end
