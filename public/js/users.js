31e51b2bb8c240e38223f6af6eda03bb464bbc63
mmazanec22 <melanie.mazanec@gmail.com>
2016-08-11 14:38:49 -0500

    Start js for user profile edit

---
+++ public/js/users.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".edit-button").on("submit", function(event){
+    event.preventDefault();
+
+    //show fields to edit in the profile
+    var url
+    var method
+    var data
+
+    var editRequest
+  })
+})
