2f62a561feb3c7c0698455e495b334ff312aca98
Kunal Patel <kunalpatelx92@gmail.com>
2016-08-11 11:13:52 -0500

    Add voting functionality for answers to a question

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,13 +1,25 @@
 post '/votes' do
-	@question = Question.find(params[:question])
-	p @question
-	if params[:upvote]
-		puts "upvote"
-		Vote.create(value: 1, votable_type: "Question", votable_id: @question.id, voter_id: 7)
-		redirect "/questions/#{@question.id}"
-	elsif params[:downvote]
-		puts "downvote"
-		Vote.create(value: -1, votable_type: "Question", votable_id: @question.id, voter_id: 7)
-		redirect "/questions/#{@question.id}"
+	if params[:question]
+		@question = Question.find(params[:question])
+		if params[:upvote]
+			puts "upvote"
+			Vote.create(value: 1, votable_type: "Question", votable_id: @question.id, voter_id: 7)
+			redirect "/questions/#{@question.id}"
+		elsif params[:downvote]
+			puts "downvote"
+			Vote.create(value: -1, votable_type: "Question", votable_id: @question.id, voter_id: 7)
+			redirect "/questions/#{@question.id}"
+		end
+	elsif params[:answer]
+		@answer = Answer.find(params[:answer])
+		if params[:upvote]
+			puts "upvote"
+			Vote.create(value: 1, votable_type: "Answer", votable_id: @answer.id, voter_id: 7)
+			redirect "/questions/#{@answer.question_id}"
+		elsif
+			puts "downvote"
+			Vote.create(value: -1, votable_type: "Answer", votable_id: @answer.id, voter_id: 7)
+			redirect "/questions/#{@answer.question_id}"
+		end
 	end
 end
